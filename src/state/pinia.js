6ed2759e9c6f2123e090bfa26a7495d8b70da5c3
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2025-02-10 13:51:56 +0700

    initial commit

---
+++ src/state/pinia.js
@@ -0,0 +1,9 @@
+import { useLayoutStore } from "./pinia/layout";
+import { useAuthStore } from "./pinia/auth";
+import { useUserStore } from "./pinia/user";
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
+export default pinia;
+
+export { useLayoutStore, useAuthStore, useUserStore };
